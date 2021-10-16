fe4b4e535b7d8390c8ea0c662df48a54b2af8734
Ram Singh <rams48705@gmail.com>
2021-10-16 20:55:32 +0530

    Another Commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and Redux and Making Project
+          Learn React and Redux and Making Project by some commits
         </a>
       </header>
     </div>
